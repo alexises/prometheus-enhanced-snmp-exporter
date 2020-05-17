bdef4b5cf17101fff7e024f22ff44feccb9cef43
Alexis Lameire <alexis.lameire@gmail.com>
2020-05-17 22:41:39 +0200

    add broken setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='prometheus-enhanced-snmp-exporter',
+    version='0.1dev',
+    packages=['prometheus_enhanced_snmp_exporter',],
+    scripts=['bin/prometheus-enhanced-snmp-exporter'],
+    license='GPLv3',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "pyramid >= 1.5",
+        "pysnmp >= 4.2",
+        "APScheduler >= 3.5",
+        "PyYAML >= 3.11"
+    ]
+)
