709cade851ea6fb1ec241f499c2191a6ab3a40e0
Malcolm COUDRAY <malcolm.coudray@cegedim.com>
2022-06-09 10:11:04 +0200

    create group

--- create_service.sh
+++ create_service.sh
@@ -7,10 +7,17 @@ SYSTEMD_SERVICE_UNIT_FILE="prometheus-enhanced-snmp-exporter.service"
 # The unit file is copied via setup.py data_files
 chown root:root /lib/systemd/system/${SYSTEMD_SERVICE_UNIT_FILE}
 
+if ! $(getent group prometheus-enhanced-snmp-exporter >/dev/null)
+then
+  echo "> Create group prometheus-enhanced-snmp-exporter"
+  groupadd prometheus-enhanced-snmp-exporter
+fi
+
 if ! $(getent passwd prometheus-enhanced-snmp-exporter >/dev/null)
 then
+  groupadd prometheus-enhanced-snmp-exporter
   echo "> Add user prometheus-enhanced-snmp-exporter"
-  useradd -Ur "prometheus-enhanced-snmp-exporter"
+  useradd -Ur prometheus-enhanced-snmp-exporter
 fi
 
 echo "> Update right on /etc/prometheus-enhanced-snmp-exporter/*"
