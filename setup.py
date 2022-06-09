4fec370c39d6c4e335c8a44efad7dab3ceb5e53a
alameire <alexis.lameire@cegedim.com>
2022-06-09 17:29:22 +0200

    more fix

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class InstallSystemdService(install):
 
 setup(
     name='prometheus-enhanced-snmp-exporter',
-    version='0.2alpha5',
+    version='0.2alpha6',
     packages=['prometheus_enhanced_snmp_exporter'],
     scripts=['bin/prometheus-enhanced-snmp-exporter'],
     license='GPLv3',
